04390686b5725664e608a657005cea07656e1fab
alkasah <alkasah485@gmail.com>
2022-11-05 13:19:18 -0700

    updation in seed

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,12 +1,16 @@
 const db = require('../config/connection');
 const { User, Closet, Article } = require('../models');
 const seedUser = require('./userData.json');
+//console.log(seedUser);
 const seedCloset = require('./closetData.json');
 const seedArticles = require('./articleData.json');
 
 db.once('open', async() => {
     try {
-    // await User.deleteMany({})     
+     await User.deleteMany({}) ;
+     await Closet.deleteMany({}) ;
+     await Article.deleteMany({}) ;   
+
     await User.create(seedUser);
     await Closet.create(seedCloset);
     await Article.create(seedArticles);
